4d45d7604845799b4e2bc497931d95e6a6fab02f
Ricky Lopez <rlopez@burroak.com>
2023-04-19 16:25:01 -0400

    [FIX] Requested changes fixed

--- boti/oak_helpdesk/models/helpdesk_ticket.py
+++ boti/oak_helpdesk/models/helpdesk_ticket.py
@@ -14,21 +14,3 @@ class HelpdeskTicket(models.Model):
 
     # add tracking to ticket_type
     ticket_type_id = fields.Many2one("helpdesk.ticket.type", tracking=True)
-
-    def _notify_get_groups(self, msg_vals=None):
-        # Original method is from portal.mixin, and is extended by helpdesk.ticket
-        groups = super(HelpdeskTicket, self)._notify_get_groups(msg_vals=msg_vals)
-        # All mail notified followers will see the Ticket link using the access token
-        self.ensure_one()
-        new_groups = []
-        for group_name, _group_method, group_data in groups:
-            new_group = (group_name, _group_method, group_data)
-            if group_name == "portal_customer":
-                group_data["has_button_access"] = True
-                new_group = (
-                    group_name,
-                    lambda pdata: pdata["id"] in self.message_partner_ids.ids,
-                    group_data,
-                )
-            new_groups.append(new_group)
-        return new_groups
